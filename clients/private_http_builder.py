e8831d428bd675962f555f1c2a41543db279b860
Vasiliy Yamutin <iamutin@yandex.ru>
2025-04-24 21:40:35 +0300

    Рефакторинг и подготовка к написанию API автотестов. Практика реализации фикстур

--- clients/private_http_builder.py
+++ clients/private_http_builder.py
@@ -1,14 +1,18 @@
+from functools import lru_cache
+
 from httpx import Client
 from pydantic import BaseModel
 
-from .authentication.authentication_client import get_authentication_client
-from .authentication.authentication_schema import LoginRequestSchema
+from clients.authentication.authentication_client import get_authentication_client
+from clients.authentication.authentication_schema import LoginRequestSchema
 
-class AuthenticationUserSchema(BaseModel):
+
+class AuthenticationUserSchema(BaseModel, frozen=True):
     email: str
     password: str
 
 
+@lru_cache(maxsize=None)  # Кешируем возвращаемое значение
 def get_private_http_client(user: AuthenticationUserSchema) -> Client:
     authentication_client = get_authentication_client()
 
